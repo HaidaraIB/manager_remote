5349cc81fb7775bbbe94eb398e740223a620e0bb
Haidara <haidaraibrahim963938@gmail.com>
2024-06-30 22:09:36 +0300

    update get withdraw amount filters

--- worker/check_withdraw/check_withdraw.py
+++ worker/check_withdraw/check_withdraw.py
@@ -295,7 +295,7 @@ get_withdraw_order_amount_handler = CallbackQueryHandler(
     pattern="^send_withdraw_order",
 )
 send_withdraw_order_handler = MessageHandler(
-    filters=filters.Regex("^\d+.?\d*$"),
+    filters=filters.REPLY & filters.Regex("^\d+.?\d*$") & Withdraw(),
     callback=send_withdraw_order,
 )
 decline_withdraw_order_handler = CallbackQueryHandler(
