bcf18037317f099917a25e26e84178812ec62e45
Haidara <haidaraibrahim963938@gmail.com>
2024-10-17 13:05:13 +0300

    bug fix

--- admin/admin_calls/common.py
+++ admin/admin_calls/common.py
@@ -21,41 +21,41 @@ def build_turn_user_calls_on_or_off_keyboard(context: ContextTypes.DEFAULT_TYPE)
     keyboard = [
         [
             InlineKeyboardButton(
-                text=f"سحب {on_off_dict[user_calls_on_off_dict['withdraw']]}",
+                text=f"سحب {on_off_dict[user_calls_on_off_dict.get('withdraw', True)]}",
                 callback_data="on_off withdraw",
             )
         ],
         [
             InlineKeyboardButton(
-                text=f"إيداع {on_off_dict[user_calls_on_off_dict['deposit']]}",
+                text=f"إيداع {on_off_dict[user_calls_on_off_dict.get('deposit', True)]}",
                 callback_data="on_off deposit",
             )
         ],
         [
             InlineKeyboardButton(
-                text=f"إنشاء حساب موثق {on_off_dict[user_calls_on_off_dict['create account']]}",
+                text=f"إنشاء حساب موثق {on_off_dict[user_calls_on_off_dict.get('create account', True)]}",
                 callback_data="on_off create account",
             ),
             InlineKeyboardButton(
-                text=f"حذف حساب {on_off_dict[user_calls_on_off_dict['delete account']]}",
+                text=f"حذف حساب {on_off_dict[user_calls_on_off_dict.get('delete account', True)]}",
                 callback_data="on_off delete account",
             ),
         ],
         [
             InlineKeyboardButton(
-                text=f"شراء USDT {on_off_dict[user_calls_on_off_dict['busdt']]}",
+                text=f"شراء USDT {on_off_dict[user_calls_on_off_dict.get('busdt', True)]}",
                 callback_data="on_off busdt",
             )
         ],
         [
             InlineKeyboardButton(
-                text=f"إنشاء شكوى {on_off_dict[user_calls_on_off_dict['make complaint']]}",
+                text=f"إنشاء شكوى {on_off_dict[user_calls_on_off_dict.get('make complaint', True)]}",
                 callback_data="on_off make complaint",
             )
         ],
         [
             InlineKeyboardButton(
-                text=f"عملك معنا {on_off_dict[user_calls_on_off_dict['work with us']]}",
+                text=f"عملك معنا {on_off_dict[user_calls_on_off_dict.get('work with us', True)]}",
                 callback_data="on_off work with us",
             )
         ],
