559db8fb0999e6f35df287f73e0289f814c6161a
Haidara <haidaraibrahim963938@gmail.com>
2024-11-22 14:05:19 +0300

    fixed a typo

--- handlers.py
+++ handlers.py
@@ -367,7 +367,7 @@ def main():
     app.job_queue.run_daily(
         callback=schedule_ghafla_offer_jobs,
         time=datetime.time(0, 0, tzinfo=TIMEZONE),
-        days=(1,3,4,6), # monday, wednesday, thursday, saturday
+        days=(1, 3, 4, 6),  # monday, wednesday, thursday, saturday
         name="schedule_ghafla_offer_jobs",
         job_kwargs={
             "id": "schedule_ghafla_offer_jobs",
@@ -380,7 +380,7 @@ def main():
     app.job_queue.run_daily(
         callback=schedule_lucky_hour_jobs,
         time=datetime.time(0, 0, tzinfo=TIMEZONE),
-        days=(0,2,4,5), # sunday, tuesday, thursday, firday
+        days=(0, 2, 4, 5),  # sunday, tuesday, thursday, friday
         name="schedule_lucky_hour_jobs",
         job_kwargs={
             "id": "schedule_lucky_hour_jobs",
